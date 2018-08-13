454bb74a9e848608ac57fcbec2157f1faf6b6db2
adelsondias <adelsondias@live.com>
2018-08-13 20:08:02 +0100

    tests with gradient boosting (better)

--- examples/ingestion_v0.py
+++ examples/ingestion_v0.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0,'../src')
 from v0 import *
 import pandas as pd
 import time
@@ -9,14 +11,14 @@ metadata0, sfeat, sensors0 = load_data(SHAPE_FOLDER, DATA_FOLDER)
 print('data loaded -',time.time()-t0)
 
 variables =  {
-        'sensors':['NO2','Temperature','O3','CO'],
-        'exogenous':['primary','trunk','motorway','traffic_signals','day','dow','hour']
+        'sensors':['NO2','Temperature','O3','PM2.5','NO','Pressure','Wind Direction'],
+        'exogenous':['primary','trunk','motorway','construction','residential','traffic_signals','crossing','bus_stop','day','dow','hour']
 }
 
 sensors, metadata = resampling_sensors(sensors0, metadata0, variables, 'H')
 print('sensors resampled -',time.time()-t0)
 print(sensors.shape)
 zx, zi = ingestion(sensors, metadata, sfeat, variables, 5, 'NO2', 'randomized')
-zx.to_csv(DATA_FOLDER+'zx.csv')
-zi.to_csv(DATA_FOLDER+'zi.csv')
+zx.to_csv(DATA_FOLDER+'zx_extended.csv')
+zi.to_csv(DATA_FOLDER+'zi_extended.csv')
 print('ingestion completed -',time.time()-t0)
